be92012b672a80ec0674af554640ad2f6840810f
kagurazakakotori <kagurazakakotori@gmail.com>
2018-04-27 00:11:21 +0800

    cpu: add registers

---
+++ CPU.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "DataType.h"
+
+struct hw_reg
+{
+	BYTE a = 0;
+	BYTE b = 0;
+	BYTE c = 0;
+	BYTE d = 0;
+	BYTE e = 0;
+	BYTE f = 0;
+	BYTE h = 0;
+	BYTE l = 0;
+	WORD sp = 0;
+	WORD pc = 0;
+};
+
+class hw_cpu
+{
+public:
+
+private:
+	hw_reg reg;
+};
