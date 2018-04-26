4e15e2ba117d1379648823ed437f1cdff4c15a24
kagurazakakotori <kagurazakakotori@gmail.com>
2018-04-27 00:54:10 +0800

    cpu: init() function and better register struct

--- CPU.h
+++ CPU.h
@@ -4,11 +4,11 @@
 struct hw_reg
 {
 	BYTE a = 0;
+	BYTE f = 0;
 	BYTE b = 0;
 	BYTE c = 0;
 	BYTE d = 0;
 	BYTE e = 0;
-	BYTE f = 0;
 	BYTE h = 0;
 	BYTE l = 0;
 	WORD sp = 0;
@@ -18,7 +18,7 @@ struct hw_reg
 class hw_cpu
 {
 public:
-
+	void init();
 private:
 	hw_reg reg;
 };
